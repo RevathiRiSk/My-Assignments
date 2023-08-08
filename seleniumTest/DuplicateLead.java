839ba7e7d22519631e8b4415f7269b2e97d9e8bb
RevathiRiSk <140264157+RevathiRiSk@users.noreply.github.com>
2023-08-07 22:40:24 -0400

    Duplicate_Lead.java

--- seleniumTest/DuplicateLead.java
+++ seleniumTest/DuplicateLead.java
@@ -33,25 +33,35 @@ public class DuplicateLead {
 		
 		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
 		
-		String createdLeadName = driver.findElement(By.linkText("Revathi")).getText();
-		String createdLeadName1 =driver.getTitle();
+		String createdLeadName = driver.findElement(By.xpath("//a[text()='Revathi']")).getText();
+		System.out.println("Name of the First Resulting Lead : " +createdLeadName);
 		
 		driver.findElement(By.xpath("//a[text() = 'Revathi']")).click();
 		
 		driver.findElement(By.linkText("Duplicate Lead")).click();
+		Thread.sleep(4000);
+		
+		String leadPage_expected = "Duplicate Lead | opentabs CRM";
+		String duplicateLeadPage = driver.getTitle();
+		
+		
+		if (duplicateLeadPage.equals(leadPage_expected)) {
+			
+			System.out.println("Page title verification successful. ");
+		}else {
+			System.out.println("Page title verification is unsuccessful.");
+		}
 		
 		driver.findElement(By.name("submitButton")).click();
 		
-		String duplicateLeadName = driver.findElement(By.linkText("Revathi")).getText();
-		String duplicateLeadName1 = driver.getTitle();
+		String nameDuplicate = driver.findElement(By.id("viewLead_firstName_sp")).getText();
 		
-		if (createdLeadName1 == duplicateLeadName1) {
-			
-			System.out.println("Both Lead Accounts are Same");
-		}else {
-			System.out.println("They both are Different");
+		if(createdLeadName.equals(nameDuplicate)) {
+			System.out.println("Duplicating is successfull");
 		}
-
+		else {
+			System.out.println("Duplicating is unsuccessfull");
+		}
 		driver.close();
 		
 	}
